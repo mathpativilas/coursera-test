7a2d609eb46e049f6c5b5f78874beef51448c3c3
vilasmathpati <84265150+vilasmathpati@users.noreply.github.com>
2021-05-29 13:35:12 +0400

    Add files via upload

---
+++ Model4_solution/script.js
@@ -0,0 +1,19 @@
+(function () {
+
+var names = ["Paula", "Laura", "Jen", "Jason", "Paul", "Jim", "Larry", "Yaakov", "John", "Frank"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
+
+
+
